dea5d1acb24d924cf812826cad1888eb087ae447
camilo <juank.yoh.117@gmail.com>
2025-06-17 16:42:27 -0500

    Initial commit

---
+++ Rick&Morty/Rick&Morty/Rick_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyApp.swift
+//  Rick&Morty
+//
+//  Created by Juan Camilo Mendieta Hernández on 17/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
